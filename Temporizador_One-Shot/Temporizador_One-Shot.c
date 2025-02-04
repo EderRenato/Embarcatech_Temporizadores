666970c9afcd94e125030a45128ff61c546af7ac
EderRenato <ederrenatosccasar@gmail.com>
2025-02-04 19:02:21 -0300

    Aplicação de pequenas correções

--- Temporizador_One-Shot/Temporizador_One-Shot.c
+++ Temporizador_One-Shot/Temporizador_One-Shot.c
@@ -11,15 +11,15 @@ const uint TIME_DELAY = 3000;
 
 volatile bool sequence_active = false;
 
-int desliga_vermelho_callback(alarm_id_t id, void *user_data);
-int desliga_azul_callback(alarm_id_t id, void *user_data);
-int desliga_todos_callback(alarm_id_t id, void *user_data);
+int64_t desliga_vermelho_callback(alarm_id_t id, void *user_data);
+int64_t desliga_azul_callback(alarm_id_t id, void *user_data);
+int64_t desliga_todos_callback(alarm_id_t id, void *user_data);
 
 
 
 bool botao_pressionado(){
     if (!gpio_get(BUTTON_PIN)){
-        sleep_ms(100);
+        sleep_ms(50);
         if (!gpio_get(BUTTON_PIN)){
             return true;
         }
@@ -35,9 +35,9 @@ int main()
     gpio_init(BLUE_PIN);
     gpio_init(RED_PIN);
     // Definição dos pinos como saída:
-    gpio_set_dir(GREEN_PIN, true);
-    gpio_set_dir(BLUE_PIN, true);
-    gpio_set_dir(RED_PIN, true);
+    gpio_set_dir(GREEN_PIN, GPIO_OUT);
+    gpio_set_dir(BLUE_PIN, GPIO_OUT);
+    gpio_set_dir(RED_PIN, GPIO_OUT);
     // Inicializa o botão como entrada com pull-up ativado
     gpio_init(BUTTON_PIN);
     gpio_set_dir(BUTTON_PIN, GPIO_IN);
@@ -57,19 +57,19 @@ int main()
     return 0;
 }
 
-int desliga_vermelho_callback(alarm_id_t id, void *user_data){
+int64_t desliga_vermelho_callback(alarm_id_t id, void *user_data){
     gpio_put(RED_PIN, false); // Desliga o vermelho
     add_alarm_in_ms(TIME_DELAY, desliga_azul_callback, NULL, false);
     return 0;
 }
 
-int desliga_azul_callback(alarm_id_t id, void *user_data){
+int64_t desliga_azul_callback(alarm_id_t id, void *user_data){
     gpio_put(BLUE_PIN, false); //Desliga o azul
     add_alarm_in_ms(TIME_DELAY, desliga_todos_callback, NULL, false);
     return 0;
 }
 
-int desliga_todos_callback(alarm_id_t id, void *user_data){
+int64_t desliga_todos_callback(alarm_id_t id, void *user_data){
     gpio_put(GREEN_PIN, false); //desliga o verde
     sequence_active = false; // Permite novo uso do botão
     return 0;
